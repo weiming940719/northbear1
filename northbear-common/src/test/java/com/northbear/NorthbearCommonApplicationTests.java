3c6ec2f95e99881aa18458ea3d9742dc71f5dd44
shijiaqi <984652185@qq.com>
2019-06-26 11:27:55 +0800

    结构优化

--- northbear-common/src/test/java/com/northbear/NorthbearCommonApplicationTests.java
+++ northbear-common/src/test/java/com/northbear/NorthbearCommonApplicationTests.java
@@ -1,9 +1,12 @@
 package com.northbear;
 
+import ch.qos.logback.core.util.FileUtil;
+import org.apache.commons.collections4.ListUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.StringUtils;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
