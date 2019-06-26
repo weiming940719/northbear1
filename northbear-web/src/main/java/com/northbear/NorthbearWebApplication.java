aa55e515ab2354c8df69f35a3c9b4cf021de60dd
shijiaqi <984652185@qq.com>
2019-06-26 11:07:11 +0800

    新增web模块

---
+++ northbear-web/src/main/java/com/northbear/NorthbearWebApplication.java
@@ -0,0 +1,13 @@
+package com.northbear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NorthbearWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NorthbearWebApplication.class, args);
+    }
+
+}
