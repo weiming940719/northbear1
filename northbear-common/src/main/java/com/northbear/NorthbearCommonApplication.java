ced1ddcff3c59112b6258bd8b31eeb00d61af9fc
shijiaqi <984652185@qq.com>
2019-06-26 11:02:59 +0800

    新增common模块

---
+++ northbear-common/src/main/java/com/northbear/NorthbearCommonApplication.java
@@ -0,0 +1,13 @@
+package com.northbear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NorthbearCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NorthbearCommonApplication.class, args);
+    }
+
+}
