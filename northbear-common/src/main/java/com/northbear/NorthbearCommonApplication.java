2a63ce2c0114a9700e75c5310bad536099c61f8d
shijiaqi <984652185@qq.com>
2019-06-26 11:09:09 +0800

    结构优化

--- northbear-common/src/main/java/com/northbear/NorthbearCommonApplication.java
+++ northbear-common/src/main/java/com/northbear/NorthbearCommonApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NorthbearCommonApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(NorthbearCommonApplication.class, args);
     }
 
 }
